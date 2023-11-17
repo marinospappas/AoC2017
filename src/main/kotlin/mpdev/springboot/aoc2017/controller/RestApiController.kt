728f1d2f92db64f1b3ad56e7f7403c3b1c21474b
marinos <marinosch.99981@gmail.com>
2023-11-17 20:25:55 +0100

    Day 01 completed

--- src/main/kotlin/mpdev/springboot/aoc2017/controller/RestApiController.kt
+++ src/main/kotlin/mpdev/springboot/aoc2017/controller/RestApiController.kt
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/aoc2023")
+@RequestMapping("/aoc2017")
 class RestApiController(@Autowired var puzzleSolvers: Map<Int, PuzzleSolver>) {
 
     @GetMapping("/day/{day}", produces = ["application/json"])
